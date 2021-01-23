98cf2c773df798b2e85142fc400a955ff5966039
Mohammed Essam <dev.mohammed.essam@gmial.com>
2021-01-23 15:09:33 +0200

    finish resolving conflicts

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -16,11 +16,8 @@ const {
   LocalStorageSession,
   Api,
 } = require("../services/gramjs");
-<<<<<<< HEAD
 const User = require("../models/User");
-=======
 const { Telegram } = require("../utils/Telegram");
->>>>>>> 8e17b0b7689db57dafc2a62465354a52d77c2d58
 let transformNumber = (number) => {
   //011x -> 3
   let firstXIndex = number.indexOf("x");
@@ -58,116 +55,6 @@ let getStaticDynamicNumberRange = (from, to) => {
   };
 };
 
-
-<<<<<<< HEAD
-  if (numbers.length == 0) {
-    return [];
-  }
-
-  let random = cuid();
-  const stringSession = new StringSession(token);
-  let client;
-  try {
-    let result = 2;
-    let retry = async () => {
-      result = await Promise.race([
-        new TelegramClient(stringSession, apiId, apiHash),
-        wait(2),
-      ]);
-      //  console.log("RETRY" , result)
-      if (result == 2) {
-        await retry();
-      }
-    };
-    await retry();
-    console.log("AFTER RETRY");
-    client = result;
-  } catch (e) {
-    console.log("ERROR ", e);
-  }
-  let retryConnect = async () => {
-    let result = await Promise.race([client.connect(), wait(2)]);
-    //  console.log("RETRY" , result)
-    console.log("RETURY REREYCONNEXT");
-    console.log(result);
-    if (result == 2) {
-      await retryConnect();
-    }
-  };
-  await retryConnect();
-  console.log("AFTER RETRY CONNECT");
-  let _numbers = numbers.map(
-    (number) =>
-      new Api.InputPhoneContact({
-        clientId: new Date().getTime(),
-        phone: number,
-        firstName: "Name" + random,
-        lastName: "Last" + random,
-      })
-  );
-  //console.log("NUMBERS ",_numbers)
-  const ImportContacts = tl.requests.contacts.ImportContacts;
-  //console.log(ImportContacts)
-
-  const filterResult = await client.invoke(
-    new ImportContacts({
-      contacts: _numbers,
-    })
-  );
-  let result = JSON.parse(JSON.stringify(filterResult)).users.map((user) => ({
-    id: user.id,
-    wasOnline: user.status && user.status.wasOnline * 1000,
-    phone: user.phone,
-  }));
-  //console.log(result)
-  internalEvent.emit("filterBulkOfNumbersEnd", new Date());
-  client.disconnect();
-  return result;
-};
-
-let filterTelegramNumbers = async (data, tokens, i = 0, hash) => {
-  // let data={
-  //     "staticPart": "201011",
-  //     "from": "800000",
-  //     "to": "900000",
-  //     "count": 100000,
-  // }
-
-  data.staticPart = data.staticPart.replace("+", "");
-  console.log("DATA ", data);
-  if (Number(data.from) > Number(data.to)) {
-    return;
-  }
-  let bulkCounter = 0;
-  let numberCounter = Number(data.from);
-  let builkOfNumbers = [];
-  console.log(
-    "Number(data.from)<=Number(data.to) ",
-    Number(data.from),
-    ":",
-    Number(data.to)
-  );
-  while (Number(data.from) <= Number(data.to) && bulkCounter < 5) {
-    bulkCounter++;
-    numberCounter++;
-    builkOfNumbers.push(`${data.staticPart}${numberCounter}`);
-  }
-  //console.log("TOKEN "+tokens[i%tokens.length])
-  //console.log("builkOfNumbers ",builkOfNumbers)
-  let _tokens = tokens[i % tokens.length];
-
-  let result = await filterBulkOfNumbers(builkOfNumbers, _tokens, hash);
-
-  console.log("RESULT ", result);
-  if (builkOfNumbers.length == 0) return;
-  event.emit("data", { result, hash });
-  // console.log("ITER NUMBER: "+i)
-  //console.log("builkOfNumbers.length: "+builkOfNumbers.length)
-  //await wait(0)
-  filterTelegramNumbers({ ...data, from: numberCounter }, tokens, ++i, hash);
-};
-=======
->>>>>>> 8e17b0b7689db57dafc2a62465354a52d77c2d58
 const generateNumbers = async (req, res, err) => {
   const { from, to } = req.body;
   const data = getStaticDynamicNumberRange(from, to);
@@ -208,7 +95,6 @@ const FilterSequence = async (req, res) => {
     //      "noTo":to
     // }
     const tokens = await Tokens.find({});
-<<<<<<< HEAD
 
     filterTelegramNumbers(
       numbers._doc,
@@ -216,8 +102,15 @@ const FilterSequence = async (req, res) => {
       0,
       cuid()
     );
-    event.on("data", async ({ results }) => {
-      console.log(results);
+
+    const telegram = new Telegram();
+    telegram.filterTelegramNumbers(
+      numbers._doc,
+      tokens.map((token) => token.token),
+      0,
+      cuid()
+    );
+    telegram.on("data", async ({ result }) => {
       try {
         const users = results.map(({ id, wasOnline, phone }) => ({
           telegramId: id,
@@ -230,25 +123,12 @@ const FilterSequence = async (req, res) => {
       } catch (error) {
         res.json({ error: error.message });
       }
-
-      //:TODO:
-      //DATA -> [{ id: 1458162226, wasOnline: 1611000837000, phone: '201100720374' }]
-      // Save DB using insertMany
-      // { id: 1458162226, wasOnline: 1611000837000, phone: '201100720374' }
+      //   //TODO:
+      //   //DATA -> [{ id: 1458162226, wasOnline: 1611000837000, phone: '201100720374' }]
+      //   // Save DB using insertMany
+      //  // { id: 1458162226, wasOnline: 1611000837000, phone: '201100720374' }
     });
-=======
-    const telegram=new Telegram()
-    telegram.filterTelegramNumbers(numbers._doc,tokens.map((token)=>token.token),0,cuid())
-    telegram.on("data",({result})=>{
-       console.log("==================",result)
-    //   //TODO: 
-    //   //DATA -> [{ id: 1458162226, wasOnline: 1611000837000, phone: '201100720374' }]
-    //   // Save DB using insertMany
-    //  // { id: 1458162226, wasOnline: 1611000837000, phone: '201100720374' }
-     
-    })
 
->>>>>>> 8e17b0b7689db57dafc2a62465354a52d77c2d58
     if (!sequenceId) res.sendStatus(400);
 
     res.json({
